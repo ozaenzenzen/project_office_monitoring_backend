e8a988b6598a95ad4dc2ba0f59028e646c546e6b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-01 22:12:00 +0700

    update routes: add platform endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -21,6 +21,8 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 	r.POST("/account/signin", controllers.SignInAccount)
 	r.POST("/account/signup", controllers.SignUpAccount)
 	r.POST("/account/editprofile", controllers.EditProfile)
+	r.POST("/platform/create", controllers.CreatePlatform)
+	r.POST("/platform/initialize", controllers.InitializePlatform)
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, HandleRoutesResponse{
@@ -28,7 +30,6 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 			Message: "Page not found",
 		})
 
-		return
 	})
 
 	return r
